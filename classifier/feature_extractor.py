75cec3b399025002073e9612814c86a4578af355
Михаил Баклашкин <mabaklashkin@sberbank.ru>
2023-06-28 15:50:33 +0300

    Updated docstring

--- classifier/feature_extractor.py
+++ classifier/feature_extractor.py
@@ -7,8 +7,20 @@ import torch.nn as nn
 class FeatureExtractor(nn.Module):
     """Applies randomly generated convolutions, then applies global max pooling &
         extracts extracts the proportion of positive values in feature maps in parallel.
+        The convolutions are only generated on init.
     """
+
     def __init__(self, input_size, num_conv, kernel_sampling_type):
+        """Initializes a FeatureExtractor object.
+
+            Args:
+                input_size: input size (length of the feature vector).
+                num_conv: number of random convolutions.
+                kernel_sampling_type: specifies the distribution to sample
+                    kernel weights from, must be one of "normal" (standard normal distribution,
+                    then centered), "binary" (random choice from [-1, 1]), or "tertiary" 
+                    (random choice from [-1, 0, 1]).
+        """
         super(FeatureExtractor, self).__init__()
         # for initialization
         self.input_size = input_size
@@ -17,6 +29,14 @@ class FeatureExtractor(nn.Module):
         self.convolutions = self._generate_random_convolutions(num_conv)
 
     def forward(self, input):
+        """Applies feature extractor to input.
+        
+            Args:
+                input: input tensor.
+
+            Returns:
+                output tensor.
+        """
         with torch.no_grad():
             # apply convolutions
             output = [conv(input) for conv in self.convolutions]
@@ -36,6 +56,17 @@ class FeatureExtractor(nn.Module):
             return output
     
     def _get_sampling_function(self, kernel_sampling_type):
+        """Returns a sampling function based on specified kernel sampling type.
+        
+            Args:
+                kernel_sampling_type: specifies the distribution to sample
+                    kernel weights from, must be one of "normal" (standard normal distribution,
+                    then centered), "binary" (random choice from [-1, 1]), or "tertiary" 
+                    (random choice from [-1, 0, 1]).
+            
+            Returns:
+                lambda function.
+        """
         if kernel_sampling_type == "normal":
             return lambda x: np.random.normal(0, 1, x)
         if kernel_sampling_type == "binary":
@@ -48,6 +79,14 @@ class FeatureExtractor(nn.Module):
         raise ValueError(message)
 
     def _generate_random_convolutions(self, num_conv):
+        """Generates a specified number of random kernels.
+
+            Args:
+                num_conv: number of convolutions to generate.
+
+            Returns:
+                list of convolutions.
+        """
         params = self._generate_random_params(num_conv)
         convolutions = [
             nn.Conv1d(1, 1, kernel_size=kernel_size, padding=padding, dilation=dilation) for
@@ -66,6 +105,14 @@ class FeatureExtractor(nn.Module):
         return convolutions
     
     def _generate_random_params(self, num_conv):
+        """Generates random parameters for a specified number of kernels.
+
+            Args:
+                num_conv: number of convolutions to generate.
+
+            Returns:
+                np.ndarray of kernel parameters.
+        """
         kernel_sizes = np.random.choice((7, 9, 11), num_conv)
         dilations = np.asarray([2 ** np.random.uniform(0, np.log2((self.input_size - 1) / (kernel_size - 1)))
                      for kernel_size in kernel_sizes])
@@ -73,12 +120,3 @@ class FeatureExtractor(nn.Module):
                     for kernel_size, dilation in zip(kernel_sizes, dilations)])
         
         return np.dstack([kernel_sizes, paddings, dilations])[0].astype(int)
-
-    def _calculate_feature_size(self):
-        return np.sum(
-            [self._calculate_conv_output_size(conv) for conv in self.convolutions]
-        )
-    
-    def _calculate_conv_output_size(self, conv):
-        return int(((self.input_size + 2 * conv.padding[0] \
-                     - conv.dilation[0] * (conv.kernel_size[0] - 1) - 1) / conv.stride[0] + 1))
--- classifier/utils.py
+++ classifier/utils.py
@@ -1,4 +1,5 @@
 def train_model(classifier, feature_extractor, scaler, inputs, labels):
+    """Trains a model on given data"""
     features = feature_extractor(inputs)
     features = features.reshape((features.shape[0], features.shape[-1])).detach().numpy()
     features = scaler.fit_transform(features)
@@ -6,6 +7,7 @@ def train_model(classifier, feature_extractor, scaler, inputs, labels):
     classifier.fit(features, labels)
 
 def test_model(classifier, feature_extractor, scaler, inputs, labels):
+    """Returns the accuracy score of a model on given data"""
     features = feature_extractor(inputs)
     features = features.reshape((features.shape[0], features.shape[-1])).detach().numpy()
     features = scaler.transform(features)
